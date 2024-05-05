353bc659c8facfcd220afc57d6f2ce9b957951cd
octavioromagnoli <octaromag@gmail.com>
2024-05-05 00:38:30 -0300

    Agrege funcion de add_padding a funciones.py

---
+++ funciones.py
@@ -0,0 +1,6 @@
+import numpy as np
+from PIL import Image
+
+def add_padding(imagen: list)->list:
+    padding = ((2, 2), (2, 2), (0, 0))
+    imagen_con_padding = np.pad(imagen, padding, mode='edge')
