f766c4e17329b56223f60aea41cc541e20ff878c
octavioromagnoli <octaromag@gmail.com>
2024-05-05 00:40:32 -0300

    Update add_padding

--- funciones.py
+++ funciones.py
@@ -4,3 +4,4 @@ from PIL import Image
 def add_padding(imagen: list)->list:
     padding = ((2, 2), (2, 2), (0, 0))
     imagen_con_padding = np.pad(imagen, padding, mode='edge')
+    return imagen_con_padding
