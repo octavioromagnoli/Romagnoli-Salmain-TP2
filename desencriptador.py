ffd51cc6dd8a8d9aed8374048d0b1389a2bc104a
fermin6 <min.salmain@gmail.com>
2024-05-07 13:12:58 -0300

    cambio len por shape em desencriptador

--- desencriptador.py
+++ desencriptador.py
@@ -1,49 +1,55 @@
 from PIL import Image
 import numpy as np
 
+def get_shape(array: list)->tuple[int, int, int]:
+    '''
+    Toma un array y devuelve una tupla con los largos de cada dimension
+    Argumentos:
+        array : np.array
+            El array del que se quiere saber la forma
+    '''
+    return array.shape
+
 def desencriptador_imagen(imagen)->list:
     secuencia = []
     matriz_imagen = np.array(imagen)
-    alto_imagen = len(matriz_imagen)
-    ancho_imagen = len(matriz_imagen[0])
-    for i in range(0, alto_imagen, 2):
-        for j in range(0, ancho_imagen, 2):
-            seccion = matriz_imagen[i: i+2, j: j+2]
+    alto_imagen, ancho_imagen, _ = get_shape(matriz_imagen)
+    for i in range(0, alto_imagen-1, 2):
+        for j in range(0, ancho_imagen-1, 2):
+            seccion = matriz_imagen[i: i+2, j: j+2, :]
             varianza = np.var(seccion, axis=(0, 1))
             var_minima = np.argmin(varianza)
-            promedio = np.mean(seccion[:,:,var_minima], axis=(0, 1))
+            promedio = int(np.mean(seccion[:,:,var_minima], axis=(0, 1)))
             verdadero_valor = (seccion[1, 1, var_minima] - promedio) 
-            if verdadero_valor < -1:
-                verdadero_valor + 256
+            if verdadero_valor < 0 and verdadero_valor != -1:
+                verdadero_valor = (verdadero_valor + 256) %256
             secuencia.append(verdadero_valor)
             if verdadero_valor == 0:
                 break
             else:
                 continue
+        if verdadero_valor == 0:
+            break
     return secuencia
 
 def desencrpitar_mensaje(secuencia)->str:
-    #Le resto a la toda la secuencia 1, excepto a los -1's  y al 0
     for i in range(len(secuencia)):
-        if secuencia[i] != -1 or secuencia[i] != 0:
+        if secuencia[i] != -1 and secuencia[i] != 0:
             secuencia[i] = secuencia[i] -1
         elif secuencia[i] == 0:
             break
 
-    #Junto los digitos que esten entre -1's
     mensaje = ""
     nueva_secuencia = []
+    
     for numero in secuencia:
         if numero == -1:
             nueva_secuencia.append(int(mensaje))
             mensaje = ""
             nueva_secuencia.append(numero)
-        elif numero == 0:
-            break
         else:
             mensaje += str(numero)
 
-    #Convierto en letras y caracteres la secuencia de numeros
     tabla_numeros_caracteres = {
     1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j',
     11: 'k', 12: 'l', 13: 'm', 14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's',
@@ -52,20 +58,19 @@ def desencrpitar_mensaje(secuencia)->str:
     38: '-', 39: '“', 40: '‘', 41: 'á', 42: 'é', 43: 'í', 44: 'ó', 45: 'ú', 46: 'ü',
     47: 'ñ'
     }
-    mensaje = []
+    mensaje = ''
     for numero in nueva_secuencia:
         if numero == 0:
             break
         elif numero == -1:
             continue
         else:
-            caracter = (tabla_numeros_caracteres[numero])
-            mensaje.append(caracter)
+            caracter = tabla_numeros_caracteres[numero]
+            mensaje += caracter
     return mensaje
 
 
 imagen = np.array(Image.open('encrypted_baboon.png'))
 secuencia = desencriptador_imagen(imagen)
+print(secuencia)
 print(desencrpitar_mensaje(secuencia))
-
-        
--- encriptador.py
+++ encriptador.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+from desencriptador import desencrpitar_mensaje
 
 def encriptador(mensaje)->list: 
     mensaje = mensaje.lower()
@@ -26,21 +27,23 @@ def mensaje_en_imagen(secuencia,imagen)->list[list[list]]:
     numero = 0
     for i in range(0, alto_imagen-1, 2):
         for j in range(0, ancho_imagen-1, 2):
-            seccion = imagen[i: i+1, j: j+1]
+            seccion = imagen[i: i+2, j: j+2]
             varianza = np.var(seccion, axis=(0, 1))
             var_minima = np.argmin(varianza)
-            promedio = np.mean(seccion[:,:,var_minima], axis=(0, 1))
+            promedio = int(np.mean(seccion[:,:,var_minima], axis=(0, 1)))
             nuevo_valor = (promedio + secuencia[numero]) %256
-            imagen[i+1][j+1][var_minima] = nuevo_valor
+            imagen[i+1,j+1,var_minima] = nuevo_valor
             if secuencia[numero] == 0:
                 break
             numero += 1
     return imagen
 
-    
 mensaje = 'Hola, como estas todos?'
 secuencia = encriptador(mensaje)
 imagen = np.array(Image.open('kuwahara_baboon.png'))
 mensaje_encriptado = mensaje_en_imagen(secuencia, imagen)
 print(secuencia)
 print(mensaje_encriptado)
+imagen = Image.fromarray(mensaje_encriptado.astype('uint8'))
+imagen.show()
+imagen.save('prueba_baboon.png')
